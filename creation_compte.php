6419c92837e3b1dba163da8091a7547c5369e9e5
victorGuyenne <victor.guyenne@gmail.com>
2025-03-14 16:23:35 +0100

    Update creation_compte.php

--- creation_compte.php
+++ creation_compte.php
@@ -1,159 +1,74 @@
-.flex1 {
-    display: flex;
-    justify-content: space-between;
-}
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion - il Casolare</title>
+    <link rel="stylesheet" href="style_connexion.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class = "flex1">
+            <div class = "flex2">
+                <h1><strong>Il Casolare</strong></h1>
+                <h2>Page de connexion |  Création de compte</h2>
+            </div>
+        </div>
+        <div class="liens">
+            <a href="a" style="color: white;" >Accueil</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="b" style="color: white;">A propos</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="c" style="color: white;">Menu</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="d" style="color: white; font-weight: 900; text-decoration: underline;">Connexion</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="e" style="color: white;">Réserver</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="f" style="color: white;">Admin</a>
+      
+          </div>
+        <nav> 
 
-.flex2 {
-    display: flex;
-    flex-direction: column;
-    margin-left: 1em;
-}
+        </nav>
+        
+	
+    </header>
 
-h1 {
-    margin-bottom: 0px;
-    color:rgba(215, 194, 87, 0.981);
-    font-family: "Dancing Script", serif;
-    font-size: 3.9em;
-    font-weight: 700;
-  font-style: normal;
-  margin-top: 20px;
-  padding-left: 30px;
-}
-
-.hr_footer {
-    display: flex;
-    margin-top: 5em;
-    margin-left: 3em;
-    margin-right: 3em;
-}
-
-.footer {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    max-width: 1000px;
-    margin-left: 3em;
-    color: rgb(112, 101, 101);
-}
-
-.flex_connexion {
-    margin-top: -50px;
-    display: flex;
-    flex-direction: column;
-    margin-right: 150px;
-    width: 100%;
-    max-width: 400px;
-    padding: 10px;
-    color: white;
-    border: 2px solid rgba(215, 194, 87, 0.981);
-    box-sizing: border-box;
-   
-
-}
-
-main {
-    display: flex;
-    justify-content: flex-end;
-    padding-top: 3em;
-}
-
-
-section {
-    margin-bottom: 20px;
-}
-
-#creation {
-    display: none;
-}
-
-h2 {
-    margin-top: 150px;
-    padding-left: 70px;
-    font-size :5.2em;
-    margin-right: 500px;
-    color: white;
-}
-
-form {
-    display: flex;
-    flex-direction: column;
-}
-
-label {
-    margin-bottom: 5px;
-}
-
-input {
-    margin-bottom: 15px;
-    padding: 10px;
-    font-size: 1em;
-    background-color:  rgb(23, 22, 22);
-    color:white;
-}
-
-.button_connexion {
-    margin-top: 10px;
-   
-    color: white;
-    background-color: rgb(23, 22, 22);
-    padding:  10px;
-    font-size: 1.2em;
-    cursor: pointer;
-    margin-top: 10px;
-}
-
-.button_creer {
-    margin-top: 10px;
-  color: white;
-  background-color: rgb(23, 22, 22);
-  padding:  10px;
-  font-size: 1.2em;
-}
-
-
-button:hover {
-    background-color: rgba(54, 53, 53, 0.375);
-}
-
-
-@media (min-width: 320px) and (max-width: 720px) {
-}
-
-
-
-.liens{
-    padding-left: 750px;
-    font-size: 1.5em;
-    margin-top: -500px;
-    margin-bottom: 150px;
+    <main>
+        <div class="flex_connexion">
+            <section id="connexion">
+                <h3>Connexion</h3>
+                <form action="connexion.php" method="post">
+                    <label for="username">Nom d'utilisateur :</label>
+                    <input type="text" id="username" name="username" required>
+                    
+                    <label for="mdp">Mot de passe :</label>
+                    <input type="password" id="mdp" name="mdp" required>
+                    
+                    <button class = "button_connexion" type="submit">Connexion</button>
+                    <button class = "button_creer" type="submit">Créer un compte</button>
+                </form>
+            </section>
     
-  }
+            <section id="creation">
+                <h2>Création de Compte</h2>
+                <form action="inscription.php" method="post">
+                    <label for="email">Email :</label>
+                    <input type="email" id="email" name="email" required>
+                    
+                    <label for="username">Nom d'utilisateur :</label>
+                    <input type="text" id="username" name="username" required>
+                    
+                    <label for="mdp">Mot de passe :</label>
+                    <input type="password" id="mdp" name="mdp" required>
 
-  a{
-    text-decoration: none;
-  }
-  a:hover{
-    font-weight: 900;
-    text-decoration: underline;
-  }
-
-  
-  h3{
-    display: flex;
-    margin-left: 100px;
-    text-decoration: underline;
-    font-family: "Dancing Script", serif;
-    font-size: 2.9em;
-    font-weight: 700;
-  font-style: normal;
-  }
-
-body{
-    background-color:  rgb(23, 22, 22);
-    background-image: url(images/cercle.png);
-    background-repeat: no-repeat;
-    background-size: 20%;
-    background-position: 10px;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-}
+                    <label for="tel">Téléphone</label>
+                    <input type="tel" id="tel" name="tel" required>
+                    
+                    <button type="submit">Créez le compte</button>
+                </form>
+            </section>
+        </div>
+    </main>
+    <script src="assets/js/script_connexion.js"></script>
+</body>
+</html>
